9efc732d5677a46928b02e56901c8a5952020040
Robbie Antenesse <gamedev@alamantus.com>
2015-09-11 10:11:56 -0600

    Untested: Added support for sqlite and pgsql databases as well.

--- php/config.php
+++ php/config.php
@@ -3,6 +3,7 @@
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]);
 define("SITE_NAME", "PHP Project");
 define("SITE_LOCATION", ROOT . "/php_project_base");    // For absolute file paths: SITE_LOCATION . "/whatever.php"
+define("DATABASE_TYPE", "mysql")   //sqlite, mysql, pgsql
 
 function connection() {
     // Fill this with relevant data.
@@ -11,11 +12,27 @@ function connection() {
     $password = "password";
     $dbname = "database_name";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    if (DATABASE_TYPE == "mysql") {
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+    } elseif (DATABASE_TYPE == "sqlite") {
+        // Create connection
+        $conn = sqlite_open($dbname, 0666, $sqliteerror);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . $sqliteerror);
+        }
+    } elseif (DATABASE_TYPE == "pgsql") {
+        // Create connection
+        $conn = pg_connect("host=" . $servername . "dbname=" . $dbname . "user=" . $username . "password=" . $password);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . pg_last_error());
+        }
     }
     
     return $conn;
--- php/helpers.php
+++ php/helpers.php
@@ -1,12 +1,33 @@
 <?php
 // Simplified PHP functions
-function query ($query) {
-    return mysqli_query(connection(), $query);
+function query ($query_string) {
+    if (DATABASE_TYPE == "mysql") {
+        $query = mysqli_query(connection(), $query_string);
+    } elseif (DATABASE_TYPE == "sqlite") {
+        $query = sqlite_query(connection(), $query_string);
+    } elseif (DATABASE_TYPE == "pgsql") {
+        $query = pg_query(connection(), $query_string);
+    }
+    return $query;
 }
 function num_rows ($query_results) {
-    return mysqli_num_rows($query_results);
+    if (DATABASE_TYPE == "mysql") {
+        $num_rows = mysqli_num_rows($query_results);
+    } elseif (DATABASE_TYPE == "sqlite") {
+        $num_rows = sqlite_num_rows($query_results);
+    } elseif (DATABASE_TYPE == "pgsql") {
+        $num_rows = pg_num_rows($query_results);
+    }
+    return $num_rows;
 }
 function fetch_assoc ($query_results) {
-    return mysqli_fetch_assoc($query_results);
+    if (DATABASE_TYPE == "mysql") {
+        $results = mysqli_fetch_assoc($query_results);
+    } elseif (DATABASE_TYPE == "sqlite") {
+        $results = sqlite_fetch_array($query_results, SQLITE_ASSOC);
+    } elseif (DATABASE_TYPE == "pgsql") {
+        $results = pg_fetch_assoc($query_results);
+    }
+    return $results;
 }
 ?>
